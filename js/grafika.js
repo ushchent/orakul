6e2022d68ae7a6362c124f7a1be97625d43840c0
ushchent <am@opendata.by>
2020-10-29 19:08:20 +0300

    Добавить круговую диаграмму

--- js/grafika.js
+++ js/grafika.js
@@ -1,6 +1,6 @@
 const margins = { top: 10, right: 30, bottom: 30, left: 50 };
-const height = 200 - margins.top - margins.bottom;
-const width = 700 - margins.left - margins.right;
+const height = 300 - margins.top - margins.bottom;
+const width = 800 - margins.left - margins.right;
 
 const x_scale = d3.scaleTime().range([margins.left, width]);
 const y_scale = d3.scaleLinear().range([height, margins.bottom]);
@@ -25,10 +25,9 @@ const line = x_scale => d3.line()
 			.y(d => y_scale(+d[1]))
 			.curve(d3.curveMonotoneX);
 
-const redraw_chart = data => {
+const redraw_line_chart = data => {
 	// Поступает Object.entries(data.line["region_id"])
 	// Вида [["2020-12-31", 23], ...] 
-	console.log("data: ", data);
 		x_scale.domain(d3.extent(data.map(d => new Date(d[0]))));
 		y_scale.domain([0, d3.max(data, d => +d[1])]);
 
@@ -42,7 +41,81 @@ const redraw_chart = data => {
 			.select("path")
 			.datum(data);
 			
-		line_chart.attr("d", line(x_scale));
+		line_chart.transition()
+			.duration(1000)
+			.attr("d", line(x_scale));
+}
+
+const redraw_pie_chart = data => {
+	// Пирожковая диаграмма
+	const verdict_map = {0: "Отказано в вынесении", 1: "Вынесен судебный приказ"};
+	console.log("pie_data: ", data);
+	    const color = d3.scaleOrdinal()
+                    .domain(Object.keys(data)) 
+                    .range(["#c2a5cf", "#008837"]);
+
+        const pie = d3.pie()(data.map(e => e[1]));
+	console.log("pie: ", pie);
+
+        const arc_generator = d3.arc()
+                            .innerRadius(0)
+                            .outerRadius(80)
+                            .startAngle(d => d.startAngle)
+                            .endAngle(d => d.endAngle);
+
+		const pie_area = d3.select(".pie");
+	
+        const arcs = pie_area.selectAll("path")
+			
+			arcs.data(pie)
+			.enter()
+            .append("path")
+            .attr("fill", function(d, i) { return color(i); })
+            .attr("stroke", "white")
+            .attr("stroke-width", 2)
+            .attr("title", function(d) { return d.data; });
+		
+		arcs.transition()
+			.duration(1000)
+			.attr("d", arc_generator)
+            .attr("fill", function(d, i) { return color(i); })
+            .attr("stroke", "white")
+            .attr("stroke-width", 1)
+            .attr("title", function(d) { return d.data; });
+
+
+		//arcs.enter()
+        //    .append("path")
+		//	.attr("class", "arc")		
+        //    .attr("fill", function(d, i) { return color(i); })
+        //    .attr("stroke", "white")
+        //    .attr("stroke-width", 2)
+        //    .attr("title", function(d) { return d.data; });
+       
+        var legend = d3.select(".pie_legend");
+
+        const legend_circles = legend.selectAll("circle")
+            .data(Object.keys(data))
+			.enter()
+            .append("circle")
+
+        legend_circles.attr("cx", 125)
+            .attr("cy", function(d, i) { return i * 20; })
+            .attr("r", 5)
+            .attr("fill", function(d, i) { return color(i); });
+
+		legend_circles.exit().remove();
+
+        const legend_labels = legend.selectAll("text")
+            .data(Object.keys(data))
+        	.enter()
+            .append("text")
+	
+        legend_labels.text(d => verdict_map[d] )
+            .attr("x", 135)
+            .attr("y",function(d, i) { return (i * 20) + 4; });	
+
+		legend_labels.exit().remove();
 }
 
 d3.json("data/data.json")
@@ -60,7 +133,8 @@ d3.json("data/data.json")
 
 		selector.on("change",  function() {
 			let value = d3.select(this).node().value;
-			redraw_chart(Object.entries(data.line[value]));
+			redraw_line_chart(Object.entries(data.line[value]));
+			redraw_pie_chart(Object.entries(data.pie[value]));
 			console.log(value);
 		});
 		const svg = d3.select("#grafika")
@@ -70,15 +144,38 @@ d3.json("data/data.json")
 		svg.append("g")
 			.attr("transform",
 				`translate( 0, ${height + margins.top} )`)
-			.attr("class", "x axis")
+			.attr("class", "x axis");
 		svg.append("g")
 			.attr("transform",
 				`translate( ${margins.left}, ${margins.top} )`)
-			.attr("class", "y axis")
+			.attr("class", "y axis");
 		svg.append("g")
 			.attr("class", "line_chart")
 			.attr("transform", "translate(0,0)")
 			.append("path");
+		
+		svg.append("g")
+			.attr("class", "pie_chart")
+			//.attr("width", "300")
+			//.attr("height", "300")
+			.attr("transform", "translate(400,0)");
+		svg.select(".pie_chart")
+			.append("g")
+			.attr("class", "pie")
+			//.attr("width", "300")
+			//.attr("height", "300")
+			.attr("transform", "translate(150, 150)")
+			.selectAll("path")
+			.data(Object.entries(data.pie[Object.entries(data.options)[0][0]]))
+			.enter()
+			.append("path")
 
-		redraw_chart(Object.entries(data.line[Object.entries(data.options)[0][0]]));
-		})
+		svg.select(".pie_chart")
+			.append("g")
+			.attr("class", "pie_legend")
+			.attr("transform", "translate(125, 60)");
+		
+		redraw_line_chart(Object.entries(data.line[Object.entries(data.options)[0][0]]));
+		redraw_pie_chart(Object.entries(data.pie[Object.entries(data.options)[0][0]]));
+})
+	
